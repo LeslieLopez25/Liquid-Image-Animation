fb0ef3635c989568519624d603107c1a3cf9465b
Leslie Lopez <leslie2515lopez@gmail.com>
2021-09-29 21:06:34 -0500

    Changed the color scheme, values of measure, and the animationo effect as well

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 new hoverEffect({
-    parent: document.querySelector(".distortion"),
-    intensity: 0.2,
-    image1: "./img1.jpg",
-    image2: "./img2.jpg",
-    displacementImage: "./fluid.jpg"
+  parent: document.querySelector(".distortion"),
+  intensity: 0.3,
+  image1: "./img1.jpg",
+  image2: "./img2.jpg",
+  displacementImage: "./ramen.jpg"
 });
